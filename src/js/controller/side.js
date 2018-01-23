50933132f773f933c55a0892977132b863b123db
Jaime Hernández <jaimevazz@gmail.com>
2018-01-23 17:57:09 -0600

    Fixed sidenav-overlay default opacity

--- src/js/controller/side.js
+++ src/js/controller/side.js
@@ -4,12 +4,12 @@ import 'materialize-css/dist/css/materialize.css';
 import '../../css/side.css';
 
 export default class Side {
-    constructor(dom){
-      this.dom = dom;
-      this.dom.sideNav();
-    }
-    toggle(show){
-      var cmd = show ? 'show':'hide';
-      this.dom.sideNav(cmd);
-    }
+  constructor(dom) {
+    this.dom = dom;
+    this.dom.sideNav();
+  }
+  toggle(show) {
+    var cmd = show ? 'show' : 'hide';
+    this.dom.sideNav(cmd);
+  }
 }
